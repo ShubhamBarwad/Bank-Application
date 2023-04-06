947a5a12ce82b1c07441ee1e4c1ce7992333212d
Shubham Barwad <sbarwad20595@gmail.com>
2023-04-06 10:07:58 +0530

    Final Commit

---
+++ Cards/src/main/java/in/bank/cards/controller/CardsController.java
@@ -0,0 +1,25 @@
+package in.bank.cards.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.bank.cards.model.Cards;
+import in.bank.cards.model.Customer;
+import in.bank.cards.repository.CardsRepository;
+
+@RestController
+public class CardsController {
+	
+	@Autowired
+	private CardsRepository cardsRepository;
+	
+	@PostMapping("/cards")
+	public List<Cards> getCardDetails(@RequestBody Customer customer) {
+		return cardsRepository.findByCustomerId(customer.getCustomerId());
+		
+	}
+}
---
+++ Cards/src/test/java/in/bank/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package in.bank.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = CardsApplication.class)
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
